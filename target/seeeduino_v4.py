8ef098b6047f08286e5baa2d3002b8a604fa6c56
Yihui Xiong <xiongyihui3@gmail.com>
2015-05-07 10:09:15 +0800

    change seeeduino v4 interface firmware, use seeed's usb vid

--- target/seeeduino_v4.py
+++ target/seeeduino_v4.py
@@ -13,6 +13,7 @@ import subprocess
 import shlex
 import time
 from serial.tools import list_ports
+from time import sleep
 
 AVRDUDE = 'external/avrdude'
 
@@ -24,54 +25,61 @@ class SeeeduinoV4():
     '''
     Seeeduino V4
     '''
-    
+
     def __init__(self, device):
         self.device = device
-    
+
     def write_interface(self):
         print('Write Seeeduino V4 interface firmware')
-        
+
         cmd = AVRDUDE + ' -c avrispmkII -P usb -p m16u2 -U lfuse:w:0xEF:m -U hfuse:w:0xD9:m -U efuse:w:0xF4:m -U flash:w:' + INTERFACE
         self.device.select_spi(1)
         time.sleep(0.1)
         args = shlex.split(cmd)
         return subprocess.check_call(args)
-        
+
     def write_bootloader(self):
         print('Write Seeeduino V4 booloader')
-        
-        
-        
+
         cmd = AVRDUDE + ' -c avrispmkII -P usb -p m328p -U lfuse:w:0xFF:m -U hfuse:w:0xDA:m -U efuse:w:0x05:m -U flash:w:' + BOOTLOADER
         self.device.select_spi(0)
         time.sleep(0.01)
         args = shlex.split(cmd)
         return subprocess.check_call(args)
-        
+
+    def find_device(self):
+        timeout = 10
+        while timeout != 0:
+            port = None
+            for p in list_ports.comports():
+                if p[2].upper().startswith('USB VID:PID=2886:0004'):
+                    port = p[0]
+                    return port
+
+            sleep(1)
+            timeout = timeout - 1
+
+        print('No Seeeduino V4 is found')
+        return None
+
     def write_test(self):
         print('Write Seeeduino V4 test program')
-        
-        port = None
-        for p in list_ports.comports():
-            if p[2].upper().startswith('USB VID:PID=2341:0043'):
-                port = p[0]
-                break
-                    
+
+        port = self.find_device()
         if not port:
-            print('NO Seeeduino V4 is found')
             return -1
-        
+
         cmd = '%s -c avrispmkII -c arduino -P %s -b 115200 -p m328p -D -U flash:w:%s' % (AVRDUDE, port, TEST_PROGRAM)
         self.device.deselect_spi()
         time.sleep(0.01)
         args = shlex.split(cmd)
         return subprocess.check_call(args)
-        
+
     def write_product(self):
        pass
-    
+
     def test(self):
-    
+
         last = self.device.read_io()
         # wait until io is stable
         while True:
@@ -79,44 +87,43 @@ class SeeeduinoV4():
             current = self.device.read_io()
             if current == last:
                 break
-            
+
             last = current
-            
+
         first = last
-        
+
         # wait until io is changed
         while True:
             current = self.device.read_io()
             if current != last:
                 break
-            
+
             last = current
-            
+
         # wait until io is stable
         while True:
             time.sleep(0.002)
             current = self.device.read_io()
             if current == last:
                 break
-            
+
             last = current
-            
+
         second = last
-            
-            
+
+
         print('IO: 0x%X -> 0x%X' % (first, second))
-        
+
         self.device.enable_dc()
         voltage = []
         for i in range(8):
             voltage.append(self.device.read_voltage(i))
-            
+
         print('Voltage: %s' % voltage)
         self.device.disable_dc()
-        
-        
+
+
         return (True, [first, second], voltage)
-        
+
     def reset(self):
         pass
-        
