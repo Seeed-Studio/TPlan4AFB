b3eee6dbe0d31feb276be23f1f1bc9d4855273ff
Yihui Xiong <xiongyihui3@gmail.com>
2015-05-08 14:31:45 +0800

    changes for new version hardware

--- tplan.py
+++ tplan.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python
- 
+
 # -*- coding: utf-8 -*-
- 
+
 import sys
 import os
 from PySide.QtGui import *
@@ -14,40 +14,63 @@ app_path = os.path.dirname(os.path.realpath(sys.argv[0]))
 
 class Bridge(QObject):
     output = Signal(str)
+    result = Signal(int, int)
     disconnected = Signal(str)
-    
+
     def __init__(self):
         QObject.__init__(self)
         self.queue = Queue.Queue()
-        
+
     def log(self, message):
         self.output.emit(message)
-        
+
     def disconnect(self, reason):
         self.disconnected.emit(reason)
-        
+
     def put(self, message):
         self.queue.put(message)
-        
+
     def get(self):
         return self.queue.get()
-        
+
     def empty(self):
         return self.queue.empty()
- 
+
+    def setBoard(self, board):
+        self.board = board
+
+    def getBoard(self):
+        return self.board
+
+    def setBoardId(self, n):
+        self.boardId = n
+
+    def getBoardId(self):
+        return self.boardId
+
+    def setBoardState(self, n, state):
+        self.result.emit(n, state)
+
 class TPlanUI(QtGui.QMainWindow, Ui_MainWindow):
     def __init__(self, bridge):
         super(TPlanUI, self).__init__(None)
         self.setupUi(self)
-        
+
         self.selectButton = self.selectButton1
         self.selectButton.setChecked(True)
-        
+        self.selectButtons = [self.selectButton1, self.selectButton2,
+                              self.selectButton3, self.selectButton4]
+
         self.bridge = bridge
-        
+
+        self.bridge.setBoard(self.boardComboBox.currentText())
+        self.bridge.setBoardId(1)
         self.bridge.output.connect(self.log)
+        self.bridge.result.connect(self.on_result)
         self.bridge.disconnected.connect(self.on_disconnected)
-        
+
+        # self.selectButton1.setStyleSheet('QPushButton {background-color: rgb(255, 0, 0);}')
+
         self.autoButton.clicked.connect(self.on_auto_button_clicked)
         self.selectButton1.clicked.connect(self.on_select_button_clicked)
         self.selectButton2.clicked.connect(self.on_select_button_clicked)
@@ -58,67 +81,81 @@ class TPlanUI(QtGui.QMainWindow, Ui_MainWindow):
         self.interfaceButton.clicked.connect(self.on_interface_button_clicked)
         self.testButton.clicked.connect(self.on_test_button_clicked)
         self.resetButton.clicked.connect(self.on_reset_button_clicked)
+        self.clearButton.clicked.connect(self.logTextEdit.clear)
 
     def log(self, text):
         self.logTextEdit.append(text)
-        
+
     def on_disconnected(self, reason):
         self.log(reason)
         if self.autoButton.text() == 'Stop':
-            self.autoButton.setText('Start')
+            self.autoButton.setText('Auto Test')
             self.manualModeGroupBox.setEnabled(True)
-            self.log('<<<<Stop auto test mode<<<<')
-                
+            self.log('<<<< Stop auto test <<<<')
+
+    def on_result(self, n, state):
+        if state == 0:
+            self.selectButtons[n].setStyleSheet('QPushButton {background-color: rgb(85, 98, 112);}')
+        if state == 1:
+            self.selectButtons[n].setStyleSheet('QPushButton {background-color: rgb(78, 205, 196);}')
+        if state == 2:
+            self.selectButtons[n].setStyleSheet('QPushButton {background-color: rgb(199, 244, 100);}')
+        if state == 3:
+            self.selectButtons[n].setStyleSheet('QPushButton {background-color: rgb(255, 107, 107);}')
+
+    def on_board_changed(self, index):
+        self.bridge.setBoard(self.boardComboBox.currentText())
+
     def on_auto_button_clicked(self):
-        if self.autoButton.text() == 'Start':
+        if self.autoButton.text() == 'Auto Test':
             self.bridge.put('start')
-            
-            self.manualModeGroupBox.setEnabled(False)
+
+            self.manualGroupBox.setEnabled(False)
             self.autoButton.setText('Stop')
-            self.log('>>>>Start auto test mode>>>>')
+            self.log('>>>> Start auto test >>>>')
         else:
             self.bridge.put('stop')
-            
-            self.autoButton.setText('Start')
-            self.manualModeGroupBox.setEnabled(True)
-            self.log('<<<<Stop auto test mode<<<<')
-        
+
+            self.autoButton.setText('Auto Test')
+            self.manualGroupBox.setEnabled(True)
+            self.log('<<<< Stop auto test <<<<')
+
     def on_select_button_clicked(self):
-        self.bridge.put('select ' + self.sender().text())
+        self.bridge.setBoardId(int(self.sender().text()))
         if self.selectButton is not self.sender():
             self.selectButton.setChecked(False)
         self.selectButton = self.sender()
         if not self.selectButton.isChecked():
             self.selectButton.setChecked(True)
-            
-        
+
+
     def on_bootloader_button_clicked(self):
-        self.bridge.put('write booloader')
-        
+        self.bridge.put('write bootloader')
+
     def on_program_button_clicked(self):
         self.bridge.put('write program')
-        
+
     def on_interface_button_clicked(self):
         self.bridge.put('write interface')
-        
+
     def on_test_button_clicked(self):
         self.bridge.put('test target')
-        
+
     def on_reset_button_clicked(self):
         self.bridge.put('reset target')
-        
+
     def closeEvent(self, event):
         self.bridge.put('quit')
         # close all threads
         event.accept()
- 
+
 if __name__ == '__main__':
     app = QtGui.QApplication(sys.argv)
     bridge = Bridge()
     message_handler = TPlanMessageHandler(bridge)
     message_handler.start()
     window = TPlanUI(bridge)
-    
+
     window.show()
     app.exec_()
     message_handler.join()
--- tplan_message_handler.py
+++ tplan_message_handler.py
@@ -10,77 +10,90 @@ class TPlanMessageHandler():
     """
      This class handle messages from UI
     """
-    
+
     def __init__(self, bridge):
-        
+
         self.bridge = bridge
         self.stop_event = threading.Event()
         self.dev = TestEquipment(self.on_dev_disconnected)
-        
+
         self.exit_auto = threading.Event()
-        
+
         self.message_thread = threading.Thread(target=self.handle_message)
-        
+
     def run_auto_test(self):
+
         while not self.exit_auto.is_set():
             if not self.dev.wait_for_target(2):
                 continue
-            
+
+            board = self.bridge.getBoard()
+            target = TARGET[board](self.dev)
             self.bridge.log('----------- search target -----------')
             start_time = time.time()
             self.dev.enable_dc()
             mask = 0
             for i in range(4):
                 if self.dev.detect_target(i):
-                    self.bridge.log('find target %d' % i)
+                    self.bridge.log('find target %d' % (i + 1))
                     mask |= 1 << i
+                    self.bridge.setBoardState(i, 1)
+                else:
+                    self.bridge.setBoardState(i, 0)
 
-            self.dev.enable_dc(False)  
-            
+            self.dev.enable_dc(False)
+
             for i in range(4):
                 if mask & (1 << i) == 0:
                     continue
-                    
+
                 self.dev.select_target(i)
 
                 time.sleep(0.1)
-                id = self.dev.read_id()
-                if id < 0:
-                    break
-                elif id == 0:
-                    continue
-                
-                self.bridge.log('>> test target %d' % i)
-                
+                # To do
+
+                self.bridge.log('>> test target %d' % (i + 1))
+                print('************** test target %d ***************' % (i + 1))
+
                 try:
-                    target = TARGET[id](self.dev)
-                    self.bridge.log('1. write interface firmware..')
+                    self.bridge.log('1. write interface firmware...')
                     target.write_interface()
-                    self.bridge.log('2. write bootloader..')
+                    self.bridge.log('ok')
+                    self.bridge.log('2. write bootloader...')
                     target.write_bootloader()
-                    
-                    time.sleep(1)
+                    self.bridge.log('ok')
+
                     self.bridge.log('3. write test program...')
-                    target.write_test()
+                    if target.write_test():
+                        self.dev.deselect_target(i)
+                        self.bridge.log('failed')
+                        self.bridge.setBoardState(i, 3)
+                        continue
+
+                    self.bridge.log('ok')
                     time.sleep(2)
                     self.bridge.log('4. read test result...')
                     result, io, voltage = target.test()
                     self.bridge.log('IO: 0x%X - 0x%X' % (io[0], io[1]))
                     self.bridge.log('Voltage: %s' % voltage)
-                    
+
                     self.bridge.log('5. write product program...')
                     target.write_product()
-                    
-                    self.bridge.log('<< test target %d done' % i)
-                    
                     self.dev.deselect_target(i)
+
+                    self.bridge.log('<< test target %d done' % (i + 1))
+
+                    self.dev.deselect_target(i)
+                    self.bridge.setBoardState(i, 2)
                 except subprocess.CalledProcessError as e:
+                    self.bridge.log('failed')
                     self.bridge.log(e)
+                    self.bridge.setBoardState(i, 3)
                     continue
-                
+
             end_time = time.time()
             self.bridge.log('time lapsed: %d' % (end_time - start_time))
-    
+
     def handle_message(self):
         while True:
             message = self.bridge.get()
@@ -96,16 +109,37 @@ class TPlanMessageHandler():
                 self.exit_auto.set()
                 self.stop_event.set()
                 break
+            else:
+                if self.dev.connect():
+                    board = self.bridge.getBoard()
+                    target = TARGET[board](self.dev)
+                    index = self.bridge.getBoardId() - 1
+                    self.dev.select_target(index)
+                    self.bridge.log('-------- %s --------' % message)
+                    try:
+                        if message == 'write interface':
+                            target.write_interface()
+                        elif message == 'write bootloader':
+                            target.write_bootloader()
+                        elif message == 'write program':
+                            target.write_test()
+
+                        self.bridge.log('ok')
+                    except subprocess.CalledProcessError as e:
+                        self.bridge.log('failed')
+                        self.bridge.log(e)
+
+                    # self.dev.deselect_target(index)
 
     def start(self):
         self.message_thread.start()
-        
+
     def join(self):
         self.exit_auto.set()
         self.stop_event.set()
         self.dev.disconnect()
         self.message_thread.join()
-        
+
     def on_dev_disconnected(self):
         self.exit_auto.set()
         self.bridge.disconnect('device is disconnected')
